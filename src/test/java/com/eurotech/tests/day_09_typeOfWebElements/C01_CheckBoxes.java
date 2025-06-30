f2c22b3ffdfda1af1cedd071e4c079ca7d65d6dc
SametEmsen <sametemsen@gmail.com>
2025-06-30 22:02:49 +0300

    day09 type of webElements (radio button checkboxes)

---
+++ src/test/java/com/eurotech/tests/day_09_typeOfWebElements/C01_CheckBoxes.java
@@ -0,0 +1,105 @@
+package com.eurotech.tests.day_09_typeOfWebElements;
+
+import com.eurotech.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class C01_CheckBoxes {
+    /**
+     * Açıklamalar   :
+     * isSelected()  ----> bir elementin seçili olup olmadığını verir  :  true/false
+     * isDisplayed()  ----> bir elementin görünür olup olmadığını verir  :  true/false
+     * isEnabled()  ----> bir element ile etkileşime (click, sendKeys vb.) geçilip geçilemeyeceğini verir  :  true/false
+     *                    isEnabled() durumunun tersi disabled ile ifade edilir  ama isDisable() metodu yoktur...
+     */
+    WebDriver driver;   // class içi global bir değişken olarak driver objesi oluşturalım   // decalaration
+
+    @BeforeMethod
+    public void setUp(){
+        driver= WebDriverFactory.getDriver("chrome");       // initialization
+      //  driver.get("http://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void checkboxTest() throws InterruptedException {
+        /**
+         * navigate to http://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html
+         * locate option 3 element
+         * verify that the element is selected
+         * click the element
+         * verify that the element is NOT selected
+         *
+         * locate option 2
+         * verify that the element is NOT selected
+         * click the element
+         * verify that the element is selected
+         */
+
+      //  WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.get("http://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+        Thread.sleep(1000);
+
+        // locate option 3 element
+        WebElement option3 = driver.findElement(By.cssSelector("[value='option-3']"));
+
+        // verify that the element is selected
+        Assert.assertTrue(option3.isSelected());
+        Thread.sleep(1000);
+
+        // click the element
+        option3.click();
+
+        Thread.sleep(1000);
+        // verify that the element is NOT selected
+        Assert.assertFalse(option3.isSelected());
+
+
+
+      //  driver.close();
+
+    }
+
+    @Test
+    public void checkboxTest02() throws InterruptedException {
+        driver.get("http://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+
+        // locate option 2
+        WebElement option2 = driver.findElement(By.xpath("//*[text()='Option 2']/input"));
+        Thread.sleep(1000);
+
+        // verify that the element is NOT selected
+        Assert.assertFalse(option2.isSelected());
+        Thread.sleep(1000);
+
+        // click the element
+        option2.click();
+        Thread.sleep(1000);
+
+        // verify that the element is selected
+        Assert.assertTrue(option2.isSelected());
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void checkboxTest_Task(){
+        /**
+         * go to https://the-internet.herokuapp.com/checkboxes
+         * locate both checkBoxes web elements
+         * verify that checkbox 1 is NOT selected
+         * verify that checkbox 2 is selected
+         * click check box 1 and check box 2
+         * verify that checkbox 1 is selected
+         * verify that checkbox 2 is NOT selected
+         */
+    }
+}
---
+++ src/test/java/com/eurotech/tests/day_09_typeOfWebElements/C02_RadioButtons.java
@@ -0,0 +1,86 @@
+package com.eurotech.tests.day_09_typeOfWebElements;
+
+import com.eurotech.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class C02_RadioButtons {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver= WebDriverFactory.getDriver("firefox");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void radioButtonTest() throws InterruptedException {
+        /**
+         * navigate to http://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html
+         * locate yellow element
+         * verify that the element is NOT selected
+         * click the element
+         * verify that the element is selected
+         *
+         * locate blue element
+         * verify that the element is displayed
+         * click the element
+         * verify that the blue element is selected
+         * verify that the yellow element is NOT selected
+         */
+
+        driver.get("http://www.webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html");
+
+        //locate yellow element
+        WebElement yellow = driver.findElement(By.cssSelector("[value='yellow']"));
+
+        // verify that the element is NOT selected
+        Assert.assertFalse(yellow.isSelected());
+        Thread.sleep(1000);
+
+        // click the element
+        yellow.click();
+
+        // verify that the element is selected
+        Assert.assertTrue(yellow.isSelected());
+        Thread.sleep(1000);
+
+        // locate blue element
+        WebElement blue=driver.findElement(By.cssSelector("[value='blue']"));
+
+        // verify that the element is displayed
+        Assert.assertTrue(blue.isDisplayed());
+        Thread.sleep(1000);
+
+        // click the element
+        blue.click();
+
+        // verify that the blue element is selected
+        Assert.assertTrue(blue.isSelected());
+
+        // verify that the yellow element is NOT selected
+        Assert.assertFalse(yellow.isSelected());
+    }
+
+    @Test
+    public void radioButtonTask() {
+        /**
+         * https://demoqa.com/automation-practice-form
+         * locate female gender element
+         * verify that the element is NOT selected
+         * verify that the element is displayed
+         * click the element
+         * verify that the element is selected
+         * ipucu: eğer elementten istediğiniz sonucu alamıyorsanız.. aynı yeri gösteren diğer elementleri deneyin....!!!!
+         */
+    }
+}
